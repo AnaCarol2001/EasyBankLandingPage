4ad04f3e4cdc3d471b26a5303ba2847b3b9e216c
AnaCarol <62663206+AnaCarol2001@users.noreply.github.com>
2024-02-27 17:33:49 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export default function App() {
+  const H1 = styled.h1`
+    font-size: 5rem;
+    text-align: center;
+  `;
+  return (
+    <>
+      <H1>Hello World</H1>
+    </>
+  );
+}
