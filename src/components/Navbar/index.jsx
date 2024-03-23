feb223a6a2e4eacd3ce4f9feb44bf9a10746f280
AnaCarol <62663206+AnaCarol2001@users.noreply.github.com>
2024-03-23 18:48:08 -0300

    refactor: updates Navbar imports and move other components/styles to its own file

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,15 +1,9 @@
 import { useState } from "react";
-import PropTypes from "prop-types";
-import { ActionBtnStyled } from "../../global-styles";
-import useMediaQuery from "../../hooks/useMediaQuery";
-import logoImg from "../../assets/images/logo.svg";
-import { motion } from "framer-motion";
-import {
-  HeaderStyled,
-  NavUlStyled,
-  NavStyled,
-  MenuBtnStyled,
-} from "./Navbar.styled";
+import useMediaQuery from "src/hooks/useMediaQuery";
+import Logo from "src/assets/images/Logo";
+import { CallToActionBtnStyled } from "src/components/Buttons/CallToAction";
+import { HeaderStyled, MenuBtnStyled } from "./Navbar.styled";
+import Navigation from "./Navigation";
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
@@ -18,7 +12,7 @@ export default function Navbar() {
     return (
       <HeaderStyled>
         <div className="wrapper">
-          <img src={logoImg} alt="Easybank" />
+          <Logo />
           <div className="nav-container">
             <Navigation isMenuOpen={isOpen} />
             <MenuBtnStyled
@@ -41,81 +35,12 @@ export default function Navbar() {
   return (
     <HeaderStyled>
       <div className="wrapper">
-        <img src={logoImg} alt="Easybank" />
+        <Logo />
         <Navigation isMenuOpen={true} />
-        <ActionBtnStyled type="button">Request Invite</ActionBtnStyled>
+        <CallToActionBtnStyled type="button">
+          Request Invite
+        </CallToActionBtnStyled>
       </div>
     </HeaderStyled>
   );
 }
-
-const variants = {
-  open: {
-    opacity: 1,
-    display: "block",
-    transition: { duration: 0.4 },
-  },
-  closed: {
-    opacity: 0,
-    transition: { duration: 0.3 },
-    transitionEnd: { display: "none" },
-  },
-};
-
-const ulVariants = {
-  open: {
-    x: "0%",
-    transition: {
-      duration: 0.5,
-      staggerChildren: 0.07,
-      delayChildren: 0.2,
-    },
-  },
-  closed: {
-    x: "100%",
-    transition: { duration: 0.3, staggerChildren: 0.07 },
-  },
-};
-
-const liVariants = {
-  open: {
-    y: 0,
-    opacity: 1,
-  },
-  closed: {
-    y: -15,
-    opacity: 0,
-  },
-};
-
-const Navigation = ({ isMenuOpen }) => {
-  return (
-    <NavStyled
-      as={motion.nav}
-      animate={isMenuOpen ? "open" : "closed"}
-      variants={variants}
-    >
-      <NavUlStyled as={motion.ul} variants={ulVariants}>
-        <motion.li variants={liVariants}>
-          <a href="#">Home</a>
-        </motion.li>
-        <motion.li variants={liVariants}>
-          <a href="#">About</a>
-        </motion.li>
-        <motion.li variants={liVariants}>
-          <a href="#">Contact</a>
-        </motion.li>
-        <motion.li variants={liVariants}>
-          <a href="#">Blog</a>
-        </motion.li>
-        <motion.li variants={liVariants}>
-          <a href="#">Careers</a>
-        </motion.li>
-      </NavUlStyled>
-    </NavStyled>
-  );
-};
-
-Navigation.propTypes = {
-  isMenuOpen: PropTypes.bool,
-};
