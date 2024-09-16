55eaeaa0a2d1b24b8493c8276eaf3d63675c3d56
AnaCarol <62663206+AnaCarol2001@users.noreply.github.com>
2024-09-16 16:36:49 -0300

    feat: adds new assets or animations to it

---
+++ src/assets/images/index.js
@@ -0,0 +1,32 @@
+import BgIntroDesk from "./BgIntroDesk";
+import { BgIntroMobile } from "./BgIntroMobile";
+import Logo from "./Logo";
+import bgIntroDesktop from "./bg-intro-desktop.svg";
+import bgIntroMobile from "./bg-intro-mobile.svg";
+import bgMobile from "./bg-mobile.svg";
+import confetti from "./image-confetti.jpg";
+import currency from "./image-currency.jpg";
+import mockups from "./image-mockups.png";
+import plane from "./image-plane.jpg";
+import restaurant from "./image-restaurant.jpg";
+import logo from "./logo.svg";
+
+const Images = {
+  component: {
+    Logo,
+    BgIntroDesk,
+    BgIntroMobile,
+  },
+  file: {
+    bgIntroDesktop,
+    bgIntroMobile,
+    bgMobile,
+    confetti,
+    currency,
+    mockups,
+    plane,
+    restaurant,
+    logo,
+  },
+};
+export default Images;
