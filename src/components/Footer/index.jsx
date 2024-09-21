5b9ef3d745d88d80dd84f314c4ac71a629b4bcb4
AnaCarol <62663206+AnaCarol2001@users.noreply.github.com>
2024-09-21 14:32:44 -0300

    !refactor: new footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,49 +1,85 @@
-import Logo from "src/assets/images/Logo";
-import { CallToActionBtnStyled } from "src/components/Buttons/CallToAction";
+import Images from "src/assets/images";
 import SocialLinks from "./SocialLinks";
-import FooterNav from "./FooterNav";
-import {
-  FooterAttributionContainer,
-  FooterContainerStyled,
-} from "./Footer.styled";
+import Nav from "../UI/Nav";
+import useMediaQuery from "src/hooks/useMediaQuery";
+import Button from "../UI/Button";
+import styled from "styled-components";
+import { NEUTRAL_COLORS, PRIMARY_COLORS } from "src/global-styles";
+
+const FooterStyled = styled.footer`
+  background-color: ${PRIMARY_COLORS.darkBlue};
+  color: ${NEUTRAL_COLORS.white};
+  display: grid;
+  justify-items: center;
+  gap: 1rem;
+  padding: 3rem 1rem;
+
+  > div {
+    display: grid;
+    place-items: center;
+    gap: 1.5rem;
+  }
+
+  p {
+    color: ${NEUTRAL_COLORS.grayBlue};
+  }
+
+  @media screen and (min-width: 768px) {
+    justify-content: space-evenly;
+    grid-template-columns: repeat(4, auto);
+    grid-template-rows: repeat(2, 1fr);
+
+    > * {
+      grid-row: span 2;
+    }
+
+    ul {
+      text-align: left;
+    }
+
+    > div {
+      align-content: space-between;
+    }
+
+    div:first-child {
+      justify-items: start;
+    }
+
+    div:last-child {
+      grid-column: 4/-1;
+      justify-items: end;
+    }
+  }
+`;
+
 export default function Footer() {
+  const isDesktop = useMediaQuery("(max-width: 767px)");
+  const navCols = isDesktop ? 1 : 2;
+  const navLinks = [
+    { title: "About Us", path: "#" },
+    { title: "Contact", path: "#" },
+    { title: "Blog", path: "#" },
+    { title: "Careers", path: "#" },
+    { title: "Support", path: "#" },
+    { title: "Privacy Policy", path: "#" },
+  ];
   return (
-    <footer>
-      <FooterContainerStyled>
-        <div className="wrapper">
-          <div className="footer__logo">
-            <Logo textFill={"#fff"} />
-          </div>
-          <div className="footer__social">
-            <SocialLinks />
-          </div>
-          <div className="footer__nav">
-            <FooterNav />
-          </div>
-          <div className="footer__action">
-            <CallToActionBtnStyled>Request Invite</CallToActionBtnStyled>
-          </div>
-          <div className="footer__copyright">
-            <p> &copy; Easybank. All Rights Reserved</p>
-          </div>
-        </div>
-      </FooterContainerStyled>
-      <FooterAttributionContainer>
-        <p>
-          Challenge by{" "}
-          <a
-            href="https://www.frontendmentor.io/challenges/easybank-landing-page-WaUhkoDN"
-            target="_blank"
-          >
-            Frontend Mentor
-          </a>
-          . Coded by{" "}
-          <a href="https://www.frontendmentor.io/profile/AnaCarol2001">
-            AnaCarolina
-          </a>
-          .
-        </p>
-      </FooterAttributionContainer>
-    </footer>
+    <FooterStyled>
+      <div>
+        <Images.component.Logo textFill={"#fff"} />
+        <SocialLinks />
+      </div>
+      <Nav.Root $gridCols={navCols} theme="dark">
+        {navLinks.map((link) => (
+          <Nav.Item key={link.title} path={link.path}>
+            {link.title}
+          </Nav.Item>
+        ))}
+      </Nav.Root>
+      <div>
+        <Button.CallToAction>Request Invite</Button.CallToAction>
+        <p> &copy; Easybank. All Rights Reserved</p>
+      </div>
+    </FooterStyled>
   );
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,15 +3,43 @@ import Navbar from "src/components/Navbar";
 import Hero from "./Hero";
 import FeaturesSection from "./Features";
 import ArticleSection from "./ArticlesSection";
+import Footer from "src/components/Footer";
+import styled from "styled-components";
+import { NEUTRAL_COLORS, PRIMARY_COLORS } from "src/global-styles";
+
+const AttributionStyled = styled.div`
+  background-color: ${PRIMARY_COLORS.darkBlue};
+  color: ${NEUTRAL_COLORS.lightGray};
+  border-top: 1px solid ${NEUTRAL_COLORS.lightGray};
+  padding: 1rem;
+  text-align: center;
+`;
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <HeadingLevelContext.Provider value={1}>
-        <Hero />
-        <FeaturesSection />
-        <ArticleSection />
+        <main>
+          <Hero />
+          <FeaturesSection />
+          <ArticleSection />
+        </main>
+        <Footer />
+        <AttributionStyled>
+          Challenge by{" "}
+          <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+            Frontend Mentor
+          </a>
+          . Coded by{" "}
+          <a
+            href="https://www.frontendmentor.io/profile/AnaCarol2001"
+            target="_black"
+          >
+            AnaCarolina
+          </a>
+          .
+        </AttributionStyled>
       </HeadingLevelContext.Provider>
     </>
   );
